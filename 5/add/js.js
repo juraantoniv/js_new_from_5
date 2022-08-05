5877cdcd181e73210ea18159684372114505f65c
juraantoniv <juraantoniv@gmail.com>
2022-08-05 13:10:46 +0300

    ok

---
+++ 5/add/js.js
@@ -0,0 +1,27 @@
+
+
+// -   функція Приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1"
+//   EXAMPLE:
+//   foo([9,8,0,4], 0) // ==> [ 8, 9, 0, 4 ]
+//   foo([9,8,0,4], 1) // ==> [ 9 ,0, 8, 4 ]
+//   foo([9,8,0,4], 2) // ==> [ 9, 8, 4, 0 ]
+//
+
+//
+// let array = [22,1,0,33,0,6,770,1,0,1]
+// let nonzero=[]
+// let zer0 = []
+// let all=[]
+// function separate(items) {
+//     for (let i = 0; i < items.length; i++) {
+//         const arrayElement = items[i];
+//         if (arrayElement !== 0) {
+//             nonzero += arrayElement + ' '
+//         } else if (arrayElement === 0) {
+//             zer0 += arrayElement + ' '
+//         }
+//     }
+//     return all = nonzero+zer0
+// }
+//
+// console.log(separate(array))
