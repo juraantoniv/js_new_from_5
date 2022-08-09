8316fb3e75a28189c733b263dae4c779bbe48f02
juraantoniv <juraantoniv@gmail.com>
2022-08-09 22:14:50 +0300

    ok

--- 6/js.js
+++ 6/js.js
@@ -9,34 +9,59 @@
 // }
 
 
+// let cards = [
+//     {cardSuit: 'heart', value: '2', color:'black'  },
+//     {cardSuit: 'clubs', value: 'Ace', color:'red'  },
+//     {cardSuit: 'clubs', value: '9', color:'red' },
+//     {cardSuit: 'clubs', value: 'Ace', color:'black'  },
+//     {cardSuit: 'heart', value: '10', color:'black'  },
+//     {cardSuit: 'clubs', value: 'Queen', color:'red'  },
+//
+// ];
+//
+// let twoColour=cards.reduce((accumulator, card)=>{
+//     if(card.color==='black'){
+//         accumulator.black.push(card)
+//     } else {
+//         accumulator.red.push(card)
+//     }
+//     return accumulator;
+// },{ red:[],  black:[] });
+//
+// console.log(twoColour)
+
+
 let cards = [
-    {cardSuit: 'heart', value: '2', color:'black'  },
+    {cardSuit: 'heart', value: '6', color:'black'  },
     {cardSuit: 'clubs', value: 'Ace', color:'red'  },
-    {cardSuit: 'clubs', value: '9', color:'red' },
+    {cardSuit: 'clubs', value: 10, color:'red' },
     {cardSuit: 'clubs', value: 'Ace', color:'black'  },
-    {cardSuit: 'heart', value: '10', color:'black'  },
+    {cardSuit: 'heart', value: 10, color:'black'  },
     {cardSuit: 'clubs', value: 'Queen', color:'red'  },
 
 ];
 
-let twoColour=cards.reduce((accumulator, card)=>{
-    if(card.color==='black'){
-        accumulator.black.push(card)
-    } else {
-        accumulator.red.push(card)
-    }
-    return accumulator;
-},{ red:[],  black:[] });
+// - знайти піковий туз
+// //
+// // let filteredCards = cards.filter(value => value.cardSuit === 'heart' && value.value === 'Ace');
+// // console.log(filteredCards);
 
-console.log(twoColour)
+// - всі шістки
+//
+// let filteredCards = cards.filter(value => value.value === '6');
+// console.log(filteredCards);
 
+// - всі червоні карти
+//
+// let filteredCards = cards.filter(value => value.color === 'red');
+// console.log(filteredCards);
 
-let cards = [
-    {cardSuit: 'heart', value: '2', color:'black'  },
-    {cardSuit: 'clubs', value: 'Ace', color:'red'  },
-    {cardSuit: 'clubs', value: '9', color:'red' },
-    {cardSuit: 'clubs', value: 'Ace', color:'black'  },
-    {cardSuit: 'heart', value: '10', color:'black'  },
-    {cardSuit: 'clubs', value: 'Queen', color:'red'  },
+// - всі буби
 
-];
+// let filteredCards = cards.filter(value => value.cardSuit === 'clubs');
+// console.log(filteredCards);
+
+// - всі трефи від 9 та більше
+
+// let filteredCards = cards.filter(value => value.value > 9);
+// console.log(filteredCards);
