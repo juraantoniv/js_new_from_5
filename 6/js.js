ec0be0bd3c815479ed813721e9046ad1fe57db0c
juraantoniv <juraantoniv@gmail.com>
2022-08-09 23:11:03 +0300

    ok

--- 6/js.js
+++ 6/js.js
@@ -30,6 +30,7 @@
 //
 // console.log(twoColour)
 
+//-------------------------------------------------------
 
 // let cards = [
 //     {cardSuit: 'heart', value: '6', color:'black'  },
@@ -45,28 +46,29 @@
 // //
 // // let filteredCards = cards.filter(value => value.cardSuit === 'heart' && value.value === 'Ace');
 // // console.log(filteredCards);
-
+//-------------------------------------------------------
 // - всі шістки
 //
 // let filteredCards = cards.filter(value => value.value === '6');
 // console.log(filteredCards);
 
+//-------------------------------------------------------
 // - всі червоні карти
 //
 // let filteredCards = cards.filter(value => value.color === 'red');
 // console.log(filteredCards);
-
+//-------------------------------------------------------
 // - всі буби
-
 // let filteredCards = cards.filter(value => value.cardSuit === 'clubs');
 // console.log(filteredCards);
 
+//-------------------------------------------------------
 // - всі трефи від 9 та більше
 
 // let filteredCards = cards.filter(value => value.value > 9);
 // console.log(filteredCards);
 
-
+//-------------------------------------------------------
 // let coursesAndDurationArray = [
 //     {title: 'JavaScript Complex', monthDuration: 5},
 //     {title: 'Java Complex', monthDuration: 6},
@@ -75,7 +77,7 @@
 //     {title: 'FullStack', monthDuration: 7},
 //     {title: 'Frontend', monthDuration: 4}
 // ];
-
+//-------------------------------------------------------
 // -- відсортувати його за спаданням за monthDuration
 //
 // let sort = coursesAndDurationArray.sort((u1, u2) => {
@@ -84,12 +86,13 @@
 //
 // console.log(sort);
 
-
+//-------------------------------------------------------
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
 
 // let filterAbd = coursesAndDurationArray.filter(value => value.monthDuration > 5);
 // console.log(filterAbd);
 
+//-------------------------------------------------------
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
 
 // let arr = [10,8,-7,55,987,-1011,0,1050,0]
@@ -98,7 +101,7 @@
 //     return {item:value.toString()};
 // });
 // console.log(arrFunk);
-
+//-------------------------------------------------------
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
 //     let nums = [11,21,3];
 // sortNums(nums,'ascending') // [3,11,21]
@@ -124,12 +127,10 @@
 //
 //
 // console.log(sortNums2);
-
-
+//-------------------------------------------------------
 // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
 //     let str = 'Ревуть воли як ясла повні';
 // let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
-
 
 // let str = 'Ревуть воли як ясла повні';
 //
@@ -139,19 +140,21 @@
 //
 // spl(str)
 
-
+//-------------------------------------------------------
 // - Знайти та вивести довижину настипних стрінгових значень
+
 //  let arr = ['hello world', 'lorem ipsum', 'javascript is cool']
 // console.log(arr[0].length)
 // console.log(arr[1].length)
 // console.log(arr[2].length)
-
+//-------------------------------------------------------
 // - Перевести до великого регістру наступні стрінгові значення
+
 //  let arr = ['hello world', 'lorem ipsum', 'javascript is cool']
 // console.log(arr[0].toUpperCase())
 // console.log(arr[1].toUpperCase())
 // console.log(arr[2].toUpperCase())
-
+//-------------------------------------------------------
 // - Перевести до нижнього регістру настипні стрінгові значення
 // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
 
@@ -159,9 +162,9 @@
 // console.log(arr[0].toLowerCase())
 // console.log(arr[1].toLowerCase())
 // console.log(arr[2].toLowerCase())
-
+//--------------------------------------------------------
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
 
+
 // let str = ' dirty string  '
-//
 // console.log(str.replaceAll(' ', ''));
