6cd743891708a6be58398132fccbd75f1b5a9749
juraantoniv <juraantoniv@gmail.com>
2022-08-09 22:41:16 +0300

    ok

--- 6/js.js
+++ 6/js.js
@@ -31,15 +31,15 @@
 // console.log(twoColour)
 
 
-let cards = [
-    {cardSuit: 'heart', value: '6', color:'black'  },
-    {cardSuit: 'clubs', value: 'Ace', color:'red'  },
-    {cardSuit: 'clubs', value: 10, color:'red' },
-    {cardSuit: 'clubs', value: 'Ace', color:'black'  },
-    {cardSuit: 'heart', value: 10, color:'black'  },
-    {cardSuit: 'clubs', value: 'Queen', color:'red'  },
-
-];
+// let cards = [
+//     {cardSuit: 'heart', value: '6', color:'black'  },
+//     {cardSuit: 'clubs', value: 'Ace', color:'red'  },
+//     {cardSuit: 'clubs', value: 10, color:'red' },
+//     {cardSuit: 'clubs', value: 'Ace', color:'black'  },
+//     {cardSuit: 'heart', value: 10, color:'black'  },
+//     {cardSuit: 'clubs', value: 'Queen', color:'red'  },
+//
+// ];
 
 // - знайти піковий туз
 // //
@@ -65,3 +65,56 @@ let cards = [
 
 // let filteredCards = cards.filter(value => value.value > 9);
 // console.log(filteredCards);
+
+
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+
+// -- відсортувати його за спаданням за monthDuration
+//
+// let sort = coursesAndDurationArray.sort((u1, u2) => {
+//     return u2.monthDuration - u1.monthDuration;
+// });
+//
+// console.log(sort);
+
+
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+
+// let filterAbd = coursesAndDurationArray.filter(value => value.monthDuration > 5);
+// console.log(filterAbd);
+
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+
+// let arr = [10,8,-7,55,987,-1011,0,1050,0]
+//
+// let arrFunk = arr.map(function (value) {
+//     return {item:value.toString()};
+// });
+// console.log(arrFunk);
+
+let nums = [11,21,3];
+
+// sortNums(nums,'ascending') // [3,11,21]
+
+let sortNums = nums.sort((u1, u2) => {
+    return u2 - u1;
+});
+
+console.log(sortNums);
+
+
+// sortNums(nums,'descending') // [21,11,3]
+
+let sortNums = nums.sort((u1, u2) => {
+    return u1 - u2;
+});
+
+
+console.log(sortNums);
